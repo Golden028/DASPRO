af1a569cc8c4aa2412a754e2daea7cd7d3f3e278
Golden028 <gotiano.pro@gmail.com>
2023-09-22 13:52:35 +0700

    commit 2

--- Semester 1/Jobsheet1/MyFirstJava11.java
+++ Semester 1/Jobsheet1/MyFirstJava11.java
@@ -1,5 +1,10 @@
 public class MyFirstJava11 {
     public static void main(String[] args) {
-        
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama        : Golden Christian Gondo");
+        System.out.println("Absen       : 11");
+        System.out.println("Kelas       : 1I");
+        System.out.println("Prodi       : Teknik Informatika");
+        System.out.println("Jurusan     : Teknologi Informasi");
     }
 }
---
+++ Semester 1/Jobsheet4/Salary11.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Salary11 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        int numAttendance, numAbsence, totalSalary;
+        int salary=40000, salaryDeduction=25000;
+        System.out.println("Input attendance number: ");
+        numAttendance = input.nextInt();
+        System.out.println("Input absence number: ");
+        numAbsence = input.nextInt();
+        totalSalary=(numAttendance*salary)-(numAbsence*salaryDeduction);
+        System.out.println("Total salary: "+totalSalary);
+    }
+}
