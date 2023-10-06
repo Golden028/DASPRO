5c7220de05c0a9c6c5d104a9c8e9ff5dd48abf97
Golden028 <gotiano.pro@gmail.com>
2023-10-06 13:50:41 +0700

    commit

---
+++ Semester 1/Jobsheet6/Selection2Exp111.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Selection2Exp111 {
+    public static void main(String[] args) {
+        int year;
+        Scanner input11 = new Scanner(System.in);
+        System.out.print("Input year = ");
+        year = input11.nextInt();
+
+        if(year%4 == 0)
+            if(year%200 != 0){
+                System.out.println("Leap year");
+            }else {
+                System.out.println("Not a leap year");
+          }else {
+            System.out.println("Not a leap year"); 
+          }
+    }
+}
---
+++ Semester 1/Jobsheet6/Selection2Exp211.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Selection2Exp211 {
+    public static void main(String[] args) {
+        int angle=0, totalAngle=0, angle1=0, angle2=0, angle3=0;
+        Scanner input11 = new Scanner(System.in);
+        System.out.print("Input angle 1 = ");
+        angle1 = input11.nextInt();
+        System.out.print("Input angle 2 = ");
+        angle2 = input11.nextInt();
+        System.out.print("Input angle 3 = ");
+        angle3 = input11.nextInt();
+        totalAngle = angle1+angle2+angle3;
+
+
+
+        if(totalAngle == 180)
+            if(angle1==90 || angle2==90 || angle3==90)
+                System.out.println("Right triangle");
+            else
+                System.out.println("Not a right trianle");
+        else
+            System.out.println("Not a triangle");
+    }
+}
