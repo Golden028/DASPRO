3290deaa77a1718f8833e63905057b588c38766e
Golden028 <gotiano.pro@gmail.com>
2023-10-13 14:19:00 +0700

    commit

---
+++ Semester 1/Jobsheet7/DoWhileLeaveEntitlement11.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class DoWhileLeaveEntitlement11 {
+    public static void main(String[] args) {
+        Scanner input11 = new Scanner(System.in);
+        int leaveEntitlement, numLeave;
+        String confirmation;
+
+        System.out.print("Number of Leave Entitlement = ");
+        leaveEntitlement = input11.nextInt();
+
+        do {
+            System.out.print("Do you want to take a leave? (y/n) = ");
+            confirmation = input11.next();
+
+            if(confirmation.equalsIgnoreCase("y")){
+                System.out.print("Leave number = ");
+                numLeave = input11.nextInt();
+
+                if (numLeave <= leaveEntitlement) {
+                    leaveEntitlement -= numLeave;
+                    System.out.println("The remaining leave entitlement = " + leaveEntitlement);
+                } else {
+                    System.out.println("The remaining leave entitlement is not sufficient!");
+                    break;
+                }
+            }
+        } while (leaveEntitlement > 0);
+    }
+}
---
+++ Semester 1/Jobsheet7/WhileOvertimePay11.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class WhileOvertimePay11 {
+    public static void main(String[] args) {
+        Scanner input11 = new Scanner(System.in);
+        int numEmployee, overtimeHours;
+        double overtimePay=0, totalOvertimePay=0;
+        String position;
+
+        System.out.print("Employee number = ");
+        numEmployee = input11.nextInt();
+
+        int i=0;
+        while (i<numEmployee){
+            System.out.print("Position of employee "+(i+1)+" (director,manager,staff) = ");
+            position = input11.next();
+            System.out.print("Employee "+(i+1)+" overtime hours = ");
+            overtimeHours = input11.nextInt();
+            i++;
+
+            if(position.equalsIgnoreCase("director")){
+                continue;
+            }else if(position.equalsIgnoreCase("manager")){
+                overtimePay=overtimeHours*100000;
+            }else if(position.equalsIgnoreCase("staff")){
+                overtimePay=overtimeHours*75000;
+            }
+
+            totalOvertimePay += overtimePay;
+        }
+        
+        System.out.println("Total of Overtime Pay = "+totalOvertimePay);
+    }
+}
