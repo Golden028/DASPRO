a664bc6500acaa1f39292cddaf600ba49158fd27
Golden028 <gotiano.pro@gmail.com>
2023-11-10 11:52:36 +0700

    commit

---
+++ Semester 1/Jobsheet9/arrayNumbers11.java
@@ -0,0 +1,9 @@
+public class arrayNumbers11 {
+    public static void main(String[] args) {
+        int[] num = {5, 13, -7, 17};
+
+        for(int i =0; i <= 4; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
