b5a41ec40123826c1869f150f20da1a6b4ea2afa
Golden028 <gotiano.pro@gmail.com>
2023-12-01 14:41:41 +0700

    commit

--- Semester 1/Jobsheet14/PowerRecursive11.java
+++ Semester 1/Jobsheet14/PowerRecursive11.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class PowerRecursive11 {
-    
+    static int calculatePower(int base, int pow){
+        if(pow==0)
+            return 1;
+        else
+        System.out.print(base+"x");
+            return base*calculatePower(base, pow-1);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input Base Number: ");
+        int base = input.nextInt();
+        System.out.print("Input Power Number: ");
+        int power = input.nextInt();
+
+
+        System.out.print("Result of "+base+" power "+power+" = ");
+        int result = calculatePower(base, power);
+        System.out.println("1 = "  + result);
+
+        input.close();
+    }
 }
